43a273c01dfcae3653cd18af2220bdc30c040f8b
Pedro Trujillo V <pedrot211@gmail.com>
2016-11-01 12:28:56 -0400

    Some changes required
    Done:
    
    - Alpha order the imports
    - Inconsistent naming.
    - String doesn't need to be an optional since all cases are accounted
    for.
    - Pass an NSIndexPath
    - alpha order variables
    - Use the sectioning comments that can be found in the other files.
    - Never use !. It can cause all kinds of problems.
    - always use self when calling a function in the current class.
    - type-o “confirm”
    - Always use self when referencing class properties.
    - imageView is only ever added once. Move this cell.imageView?.image to
    the .TreeName case.
    working in:
    
    - Alpha order functions
    - correct UI

--- Street Trees/STConfirmationPageViewController.swift
+++ Street Trees/STConfirmationPageViewController.swift
@@ -6,19 +6,20 @@
 //  Copyright © 2016 Code for Orlando. All rights reserved.
 //
 
-import UIKit
+
 import StreetTreesTransportKit
 import StreetTreesPersistentKit
+import UIKit
 
 
 //**********************************************************************************************************************
-// MARK: - Constants
+// MARK: - Constants:
 
 private let STTreeErrorTitle = "Error"
 private let STTreeErrorNameMessage = "Tree name has not been passed."
 
 //**********************************************************************************************************************
-// MARK: - Enumerations
+// MARK: - Enumerations:
 
 enum STConfirmationDetailRows
 {
@@ -27,9 +28,9 @@ enum STConfirmationDetailRows
     case CityStateZip
     case UserName
     case Phone
-    case email
+    case Email
     
-    func sectionHeader() -> String?
+    func sectionHeader() -> String
     {
         switch self
         {
@@ -37,7 +38,7 @@ enum STConfirmationDetailRows
             return "Tree Information"
         case .Address, .CityStateZip:
             return "Location"
-        case .UserName, .Phone, .email:
+        case .UserName, .Phone, .Email:
             return "Contact"
         }
     }
@@ -45,7 +46,7 @@ enum STConfirmationDetailRows
 
 
 //**********************************************************************************************************************
-// MARK: - DataSource
+// MARK: - DataSource:
 
 struct DataSource
 {
@@ -60,8 +61,8 @@ struct DataSource
         return self.dataSource[anIndex].count
     }
     
-    func item(forRow aRow: Int, inSection aSection: Int) -> STConfirmationDetailRows {
-        return self.dataSource[aSection][aRow]
+    func item(forIndexPath anIndexPath:NSIndexPath) -> STConfirmationDetailRows {
+        return self.dataSource[anIndexPath.section][anIndexPath.row]
     }
     
     init(dataSource:[[STConfirmationDetailRows]]) {
@@ -70,7 +71,7 @@ struct DataSource
 }
 
 //**********************************************************************************************************************
-// MARK: - Protocol
+// MARK: - Protocol:
 
 protocol STConfirmationPageViewControllerDelegate: NSObjectProtocol
 {
@@ -82,70 +83,70 @@ protocol STConfirmationPageViewControllerDelegate: NSObjectProtocol
 class STConfirmationPageViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, TreeDescription, Address, Contact
 {
     
-    var treeDescription:STPKTreeDescription?
     var address: STTKStreetAddress?
     var contact: STTKContact?
+    var dataSource: DataSource = DataSource(dataSource: [[.TreeName], [.Address, .CityStateZip], [.UserName, .Phone, .Email]])
+    weak var  delegate:STConfirmationPageViewControllerDelegate?
+    var treeDescription:STPKTreeDescription?
  
     
-    var dataSource: DataSource = DataSource(dataSource: [[.TreeName], [.Address, .CityStateZip], [.UserName, .Phone, .email]])
-    
-    weak var  delegate:STConfirmationPageViewControllerDelegate?
-
-    // MARK: - Table View Methods:
+//**********************************************************************************************************************
+// MARK: - Table View Methods:
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int
     {
-        return dataSource.sections
+        return self.dataSource.sections
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return dataSource.itemsForSection(atIndex: section)
+        return self.dataSource.itemsForSection(atIndex: section)
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
     {
         let cell = tableView.dequeueReusableCellWithIdentifier("emptyCell", forIndexPath: indexPath)
-
-        let kindOfRow = dataSource.item(forRow: indexPath.row, inSection: indexPath.section)
+        let kindOfRow = self.dataSource.item(forIndexPath: indexPath)
         
-        var textLabel:String? = nil
         var imageView:UIImage? = nil
+        var textLabel:String? = nil
         
         switch kindOfRow
         {
         case .TreeName:
             textLabel = treeDescription?.name ?? "Error retrieving tree name"
             imageView = treeDescription?.image() ?? UIImage(named: "blankMap")
+            cell.imageView?.image = imageView
         case .Address:
-            textLabel = (address?.streetAddress)!  + " " +  (address?.secondaryAddress)! ?? "Error retrieving address"
+            let anAddress = address?.streetAddress ?? "No address specified"
+            let anAddress2 = address?.secondaryAddress ?? ""
+            textLabel =  "\(anAddress) \(anAddress2)"
         case .CityStateZip:
             textLabel = "\(address?.city), \(address?.state) \(String((address?.zipCode)!))" ?? "Error retrieving location"
         case .UserName:
             textLabel = (contact?.name)! ?? "Error retrieving name"
         case .Phone:
-            textLabel = (contact?.phoneNumber)! ?? "Error retrieving phone number"
-        case .email:
+            textLabel = contact!.phoneNumber ?? "Error retrieving phone number"
+        case .Email:
             textLabel = (contact?.email)! ?? "Error retrieving email"
         }
         
         cell.textLabel?.text = textLabel
-        cell.imageView?.image = imageView
        
         return cell
     }
     
     func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return dataSource.item(forRow: 0, inSection: section).sectionHeader()
+        return self.dataSource.item(forIndexPath: NSIndexPath(forRow: 0, inSection: section)).sectionHeader()
     }
     
-
-    // MARK: Action handlers:
+//**********************************************************************************************************************
+// MARK: - Actions:
     
     @IBAction func confirmButtonItemTouchUpInside(sender: AnyObject)
     {
         
-        if confrim()
+        if self.confirm()
         {
             let alertConfimationMessage = UIAlertController(title: "Confirmation", message: "Thank You! Your request has been sent", preferredStyle: .Alert)
             let sentImage = UIImage(named: "sent")
@@ -156,10 +157,10 @@ class STConfirmationPageViewController: UIViewController, UITableViewDataSource,
         }
     }
     
-    //******************************************************************************************************************
-    // MARK: - Private Functions
+//******************************************************************************************************************
+// MARK: - Private Functions:
     
-    func confrim() -> Bool
+    func confirm() -> Bool
     {
         guard let treeName = treeDescription?.name where treeName.isEmpty else
         {
